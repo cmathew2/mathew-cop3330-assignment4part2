9a2b5bfc73826a112d437f8744583d233cfb969c
Chris Mathew <cmathew2@Knights.ucf.edu>
2021-11-15 14:51:02 -0500

    amend

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -22,12 +22,14 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ToDo.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.setTitle("ToDoList");
             primaryStage.show();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 }
