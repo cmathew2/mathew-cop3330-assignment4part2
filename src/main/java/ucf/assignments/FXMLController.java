ab9cae3b99756d3e041f618540975c9523c57709
Chris Mathew <cmathew2@Knights.ucf.edu>
2021-11-15 22:05:46 -0500

    amend

--- src/main/java/ucf/assignments/FXMLController.java
+++ src/main/java/ucf/assignments/FXMLController.java
@@ -1,21 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Chris Mathew
+ */
 package ucf.assignments;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class FXMLController implements Initializable {
+public class FXMLController {
 
     @FXML
-    private Label label;
+    private ResourceBundle resources;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
     }
+
 }
